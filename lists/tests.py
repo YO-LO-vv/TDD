a01547078f2268d072a643202f6ff344be6d3fe1
YO-LO-vv <919440676@qq.com>
2022-05-10 15:14:00 +0800

    fix some bugs

--- lists/tests.py
+++ lists/tests.py
@@ -1,26 +1,18 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
-from django.http import render
-from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 from lists.views import home_page
-
+##from lists.models import Item
 
 class HomePageTest(TestCase):
-	def home_page(request):
-		return render(request,'home.html',{
-			'new_item_text':request.POST.get('item_text',''),
-			})	
-
-
-	def test_uses_home_template(self):
-		response=self.client.get('/')
-		self.assertTemplateUsed(response,'home.html')
-
+    def test_uses_home_template(self):
+        response=self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
 
     def test_can_save_a_POST_request(self):
-        response=self.client.post('/',data={'item_text': 'A new list item'})
-        self.assertIn('A new list item',response.content.decode())
+        response=self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
